89b3c12fc2ab095b8dff9ba2a6548bbdf4eda9a4
Thimira Sadeesha <thimira@maximimpressions.com>
2025-10-16 23:00:28 +0530

    add testing and packge json

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,5 @@
+import logger, { LumiLogger } from './index';
 
-import logger, {LumiLogger} from "./index";
 
 logger.info('This is an info message');
 logger.warn('This is a warning');
@@ -7,7 +7,7 @@ logger.error('This is an error');
 logger.success('This is a success message');
 logger.debug('Debugging details:', { key: 'value' });
 
-// Using custom instance
+
 const customLogger = new LumiLogger({ logFile: 'custom.log', colors: false });
 customLogger.info('Custom logger info');
 customLogger.clearLogs();
