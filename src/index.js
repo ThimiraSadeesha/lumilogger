a4d5e1f0cdf9a51b36b4661072c29c1c3c0dc098
Thimira Sadeesha <thimira@maximimpressions.com>
2025-10-18 20:19:23 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,35 +1,61 @@
 "use strict";
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
     }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.logger = exports.LumiLogger = void 0;
-var fs = require("fs");
-var path = require("path");
-var LumiLogger = /** @class */ (function () {
-    function LumiLogger(config) {
-        if (config === void 0) { config = {}; }
-        this.colorReset = '\x1b[0m';
-        this.colorRed = '\x1b[31m';
-        this.colorGreen = '\x1b[32m';
-        this.colorYellow = '\x1b[33m';
-        this.colorCyan = '\x1b[36m';
-        this.colorMagenta = '\x1b[35m';
-        this.errorLabel = '[ERROR] ';
-        this.warnLabel = '[WARN] ';
-        this.infoLabel = '[INFO] ';
+const fs = __importStar(require("fs"));
+const path = __importStar(require("path"));
+class LumiLogger {
+    logDir;
+    logFile;
+    colors;
+    colorReset = '\x1b[0m';
+    colorRed = '\x1b[31m';
+    colorGreen = '\x1b[32m';
+    colorYellow = '\x1b[33m';
+    colorCyan = '\x1b[36m';
+    colorMagenta = '\x1b[35m';
+    errorLabel = '[ERROR] ';
+    warnLabel = '[WARN] ';
+    infoLabel = '[INFO] ';
+    constructor(config = {}) {
         this.logDir = config.logDir || path.join(process.cwd(), 'logs');
         this.logFile = config.logFile || 'app.log';
         this.colors = config.colors !== undefined ? config.colors : true;
         this.initLogDir();
     }
-    LumiLogger.prototype.initLogDir = function () {
+    initLogDir() {
         try {
             if (!fs.existsSync(this.logDir)) {
                 fs.mkdirSync(this.logDir, { recursive: true });
@@ -38,102 +64,74 @@ var LumiLogger = /** @class */ (function () {
         catch (error) {
             console.error(this.colorRed, 'Failed to create log directory:', error, this.colorReset);
         }
-    };
-    LumiLogger.prototype.colorize = function (color, message) {
-        return this.colors ? "".concat(color).concat(message).concat(this.colorReset) : message;
-    };
-    LumiLogger.prototype.writeToFile = function (message) {
-        var logPath = path.join(this.logDir, this.logFile);
-        var timestamp = new Date().toISOString();
-        var logEntry = "".concat(timestamp, " ").concat(message, "\n");
+    }
+    colorize(color, message) {
+        return this.colors ? `${color}${message}${this.colorReset}` : message;
+    }
+    writeToFile(message) {
+        const logPath = path.join(this.logDir, this.logFile);
+        const timestamp = new Date().toISOString();
+        const logEntry = `${timestamp} ${message}\n`;
         try {
             fs.appendFileSync(logPath, logEntry, 'utf8');
         }
         catch (error) {
             console.error(this.colorRed, 'Failed to write to log file:', error, this.colorReset);
         }
-    };
+    }
     // Error logging methods
-    LumiLogger.prototype.error = function (message) {
-        var args = [];
-        for (var _i = 1; _i < arguments.length; _i++) {
-            args[_i - 1] = arguments[_i];
-        }
-        var formattedMsg = this.formatMessage(message, args);
+    error(message, ...args) {
+        const formattedMsg = this.formatMessage(message, args);
         console.log(this.colorize(this.colorRed, formattedMsg));
         this.writeToFile(this.errorLabel + formattedMsg);
-    };
-    LumiLogger.prototype.errorFatal = function (message) {
-        var args = [];
-        for (var _i = 1; _i < arguments.length; _i++) {
-            args[_i - 1] = arguments[_i];
-        }
-        var formattedMsg = this.formatMessage(message, args);
+    }
+    errorFatal(message, ...args) {
+        const formattedMsg = this.formatMessage(message, args);
         this.writeToFile(this.errorLabel + formattedMsg);
         console.error(this.colorize(this.colorRed, formattedMsg));
         process.exit(1);
-    };
+    }
     // Success/Default logging methods
-    LumiLogger.prototype.success = function (message) {
-        var args = [];
-        for (var _i = 1; _i < arguments.length; _i++) {
-            args[_i - 1] = arguments[_i];
-        }
-        var formattedMsg = this.formatMessage(message, args);
+    success(message, ...args) {
+        const formattedMsg = this.formatMessage(message, args);
         console.log(this.colorize(this.colorGreen, formattedMsg));
         this.writeToFile(formattedMsg);
-    };
-    LumiLogger.prototype.log = function (message) {
-        var args = [];
-        for (var _i = 1; _i < arguments.length; _i++) {
-            args[_i - 1] = arguments[_i];
-        }
-        this.success.apply(this, __spreadArray([message], args, false));
-    };
+    }
+    log(message, ...args) {
+        this.success(message, ...args);
+    }
     // Warning logging methods
-    LumiLogger.prototype.warn = function (message) {
-        var args = [];
-        for (var _i = 1; _i < arguments.length; _i++) {
-            args[_i - 1] = arguments[_i];
-        }
-        var formattedMsg = this.formatMessage(message, args);
+    warn(message, ...args) {
+        const formattedMsg = this.formatMessage(message, args);
         console.log(this.colorize(this.colorYellow, formattedMsg));
         this.writeToFile(this.warnLabel + formattedMsg);
-    };
+    }
     // Info logging methods
-    LumiLogger.prototype.info = function (message) {
-        var args = [];
-        for (var _i = 1; _i < arguments.length; _i++) {
-            args[_i - 1] = arguments[_i];
-        }
-        var formattedMsg = this.formatMessage(message, args);
+    info(message, ...args) {
+        const formattedMsg = this.formatMessage(message, args);
         console.log(this.colorize(this.colorCyan, formattedMsg));
         this.writeToFile(this.infoLabel + formattedMsg);
-    };
+    }
     // Debug logging method
-    LumiLogger.prototype.debug = function (message) {
-        var args = [];
-        for (var _i = 1; _i < arguments.length; _i++) {
-            args[_i - 1] = arguments[_i];
-        }
-        var formattedMsg = this.formatMessage(message, args);
+    debug(message, ...args) {
+        const formattedMsg = this.formatMessage(message, args);
         console.log(this.colorize(this.colorMagenta, formattedMsg));
         this.writeToFile('[DEBUG] ' + formattedMsg);
-    };
-    LumiLogger.prototype.formatMessage = function (message, args) {
+    }
+    formatMessage(message, args) {
         if (args.length === 0)
             return message;
         // Simple string interpolation
-        var result = message;
-        args.forEach(function (arg) {
-            var value = typeof arg === 'object' ? JSON.stringify(arg) : String(arg);
+        let result = message;
+        args.forEach((arg) => {
+            const value = typeof arg === 'object' ? JSON.stringify(arg) : String(arg);
             result += ' ' + value;
         });
         return result;
-    };
+    }
     // Clear log file
-    LumiLogger.prototype.clearLogs = function () {
-        var logPath = path.join(this.logDir, this.logFile);
+    clearLogs() {
+        const logPath = path.join(this.logDir, this.logFile);
         try {
             if (fs.existsSync(logPath)) {
                 fs.writeFileSync(logPath, '', 'utf8');
@@ -143,15 +141,15 @@ var LumiLogger = /** @class */ (function () {
         catch (error) {
             this.error('Failed to clear log file:', error);
         }
-    };
+    }
     // Get log file path
-    LumiLogger.prototype.getLogPath = function () {
+    getLogPath() {
         return path.join(this.logDir, this.logFile);
-    };
-    return LumiLogger;
-}());
+    }
+}
 exports.LumiLogger = LumiLogger;
 // Create default instance
-var logger = new LumiLogger();
+const logger = new LumiLogger();
 exports.logger = logger;
 exports.default = logger;
+//# sourceMappingURL=index.js.map
